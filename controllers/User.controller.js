ffe463eb251709db3c4f32deff98af3c533cbcac
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-02-14 18:58:23 +0200

    Add middleware

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -21,9 +21,10 @@ module.exports.findAll = async (req, res, next) => {
 
 module.exports.findOnePK = async(req, res, next) => {
     try {
-        const {params: {id}} = req;
-        const findUser = await User.findByPk(id);
-        return res.status(200).send(findUser);
+        // const {params: {id}} = req;
+        // const findUser = await User.findByPk(id);
+        const { userInstance } = req;
+        return res.status(200).send(userInstance);
     } catch (error) {
         next(error);
     }
@@ -66,9 +67,10 @@ module.exports.deleteByPK = async(req, res, next) => {
 
 module.exports.updateUser = async(req, res, next) => {
     try {
-        const {params: {id}, body} = req;
-        const foundedUser = await User.findByPk(id);
-        const result = await foundedUser.update(body);
+        const {body} = req;
+        // const foundedUser = await User.findByPk(id);
+        const { userInstance } = req;
+        const result = await userInstance.update(body);
         return res.status(200).send(result);
     } catch (error) {
         next(error)
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
 const {Router} = require('express');
 const UserController = require('../controllers/User.controller');
 const TaskController = require('../controllers/Task.controller');
+const {getUserInstance} = require('../middlewares/user.mv');
 
 const router = Router();
 
 router.post('/user', UserController.createUser);
 router.get('/users', UserController.findAll);
-router.get('/user/:id', UserController.findOnePK);
-router.delete('/user/:id', UserController.deleteByPK);
-router.put('/user/:id', UserController.updateUser);
+router.get('/user/:userId', getUserInstance, UserController.findOnePK);
+router.delete('/user/:userId', UserController.deleteByPK);
+router.put('/user/:userId', getUserInstance, UserController.updateUser);
 
 
-router.post('/task/:userId', TaskController.createTask);
-router.get('/task/:userId', TaskController.getAllUserTasks);
-router.get('/task-count/:userId', TaskController.getCountOfTasks);
+router.post('/task/:userId', getUserInstance, TaskController.createTask);
+router.get('/task/:userId', getUserInstance, TaskController.getAllUserTasks);
+router.get('/task-count/:userId', getUserInstance, TaskController.getCountOfTasks);
 
 
 module.exports = router;
