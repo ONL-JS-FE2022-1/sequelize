f024b9adeabe18f4e2de276723bc176ca46f9286
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-02-14 19:53:03 +0200

    Add user validate and error handling

--- middlewares/user.mv.js
+++ middlewares/user.mv.js
@@ -1,4 +1,5 @@
 const {User} = require('../models');
+const { USER_SCHEMA } = require('../schemas/user.schema');
 
 module.exports.getUserInstance = async(req, res, next) => {
     try {
@@ -9,6 +10,18 @@ module.exports.getUserInstance = async(req, res, next) => {
         }
         req.userInstance = user;
         next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports.validateUser = async(req, res, next) => {
+    try {
+        const {body} = req;
+        const validated = await USER_SCHEMA.validate(body);
+        if(validated) {
+            next();
+        }
     } catch (error) {
         next(error);
     }
