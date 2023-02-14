bc6dc7d927de152f920f4ec3fe9752624aee5175
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-02-14 20:16:57 +0200

    Add task validate and custom error

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const {Router} = require('express');
 const UserController = require('../controllers/User.controller');
 const TaskController = require('../controllers/Task.controller');
 const {getUserInstance, validateUser} = require('../middlewares/user.mv');
+const {validateTask} = require('../middlewares/task.mv');
 
 const router = Router();
 
@@ -12,7 +13,7 @@ router.delete('/user/:userId', UserController.deleteByPK);
 router.put('/user/:userId', getUserInstance, UserController.updateUser);
 
 
-router.post('/task/:userId', getUserInstance, TaskController.createTask);
+router.post('/task/:userId', validateTask, getUserInstance, TaskController.createTask);
 router.get('/task/:userId', getUserInstance, TaskController.getAllUserTasks);
 router.get('/task-count/:userId', getUserInstance, TaskController.getCountOfTasks);
 
