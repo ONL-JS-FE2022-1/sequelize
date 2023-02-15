20ade6ebda20273241bbe1da1d1f4313c050ac02
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-02-15 20:18:59 +0200

    Refactor router

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,11 @@
 const {Router} = require('express');
-const UserController = require('../controllers/User.controller');
-const TaskController = require('../controllers/Task.controller');
-const GroupController = require('../controllers/Group.controller');
-const {getUserInstance, validateUser} = require('../middlewares/user.mv');
-const {validateTask} = require('../middlewares/task.mv');
+const userRouter = require('./userRouter');
+const taskRouter = require('./taskRouter');
+const groupRouter = require('./groupRouter');
 
 const router = Router();
-
-router.post('/user', validateUser, UserController.createUser);
-router.get('/users', UserController.findAll);
-router.get('/user/:userId', getUserInstance, UserController.findOnePK);
-router.get('/users/groups/:userId', UserController.getUserWithGroups);
-router.delete('/user/:userId', UserController.deleteByPK);
-router.put('/user/:userId', getUserInstance, UserController.updateUser);
-
-
-router.post('/task/:userId', validateTask, getUserInstance, TaskController.createTask);
-router.get('/task/:userId', getUserInstance, TaskController.getAllUserTasks);
-router.get('/task-count/:userId', getUserInstance, TaskController.getCountOfTasks);
-
-router.post('/groups', GroupController.createGroup);
-router.put('/groups/:userId/:groupId', getUserInstance, GroupController.addUserToGroup);
-router.get('/groups/:userId', getUserInstance, GroupController.getUserGroups);
-router.delete('/groups/:userId/:groupId', getUserInstance, GroupController.deleteUserFromGroup);
-
+router.use('/users', userRouter);
+router.use('/task', taskRouter);
+router.use('/groups', groupRouter);
 
 module.exports = router;
